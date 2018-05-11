e84646cb8372abe79404374f1474b54c4794d771
AlejandraRamirez <ma.ramirez@uniandes.edu.co>
2018-05-11 00:02:15 -0500

    Done 3.0 ?

--- AlejandraRamirez_S15C1.cpp
+++ AlejandraRamirez_S15C1.cpp
@@ -12,66 +12,70 @@ double nasty_function(double x);
 
 int main () {
 
-   int N = 100;
+   int N = 200000;
 
    vector<double> x_walk;
-   for(int i=0; i<N; i++){
+
+   //Num aleatorio entre 0 y 1
+    double a = (double) rand() / (RAND_MAX);
+
+    //Num aleatorio entre -4 y 4
+    double x_0 = 8.0*((a)-0.5);
+    //double x_0 = 3.0;
+    x_walk.push_back(x_0);
+    //cout << x_walk[1] << endl;
+
+
+   for(int k=0; k<N; k++){
 
        //Num aleatorio entre 0 y 1
        double a = (double) rand() / (RAND_MAX);
-
-       //Num aleatorio entre -4 y 4
-       double x_0 = 8.0*((a)-0.5);
-       x_walk.push_back(x_0);
-       //cout << x_walk[i] << endl;
-   }
-
-
-   for(int i=0; i<N; i++){
-
-       //Num aleatorio entre 0 y 1
-       double a = (double) rand() / (RAND_MAX);
+       //cout << a << endl;
 
        //Num aleatorio entre -0.05 y 0.05
        double paso_Al = (1/10.0)*((a)-0.5);
        //cout << paso_Al << endl;
 
        //Initial guess + paso aletario
-       double x_prime = x_walk[i] + paso_Al;
-       //cout << x_prime << endl;
+       double x_prime = x_walk[k] + paso_Al;
+       //cout << x_prime << " " << x_walk[k] << endl;
 
-       double alpha = nasty_function(x_prime)/nasty_function(x_walk[i]);
+       double alpha = nasty_function(x_prime)/nasty_function(x_walk[k]);
        //cout << alpha << endl;
 
        if(alpha >= 1.0){
+         //cout << x_prime << endl;
            x_walk.push_back(x_prime);
        }
        else{
            //Num aleatorio entre 0 y 1
            double beta = (double) rand() / (RAND_MAX);
+           //cout <<beta << endl;
            if(beta <= alpha){
                x_walk.push_back(x_prime);
+               //cout << x_prime << endl;
            }
            else{
                //Se guarda doble
-               x_walk.push_back(x_walk[i]);
+               x_walk.push_back(x_walk[k]);
+               //cout << x_walk[k] << endl;
            }
        }
    }
 
-double f[1000];
+//double f[1000];
 
 int u = 0;
 
 //Valores que se dan a python
    double i = -4.0;
-   double cc = (4.0-i)/1000;
+   double cc = (4.0-i)/200000;
    while(i<4.0){
+
+      cout << i << " " << nasty_function(i) << " " << nasty_function(x_walk[u]) << " " <<  x_walk[u] << endl;
        //f[u] = nasty_function(i);
        i = i + cc;
        u = u+1;
-
-       cout << i << " " << nasty_function(i) << " " << nasty_function(x_walk[u]) << endl;
 
    }
 
--- Graphy.py
+++ Graphy.py
@@ -4,7 +4,13 @@ import matplotlib.pyplot as plt
 x = np.genfromtxt("dato.txt", usecols = 0)
 y = np.genfromtxt("dato.txt", usecols = 1)
 d = np.genfromtxt("dato.txt", usecols = 2)
+x_walk = np.genfromtxt("dato.txt", usecols = 3)
 
+
+#f = nasty_function(x)
+norm = sum(y*(x[1]-x[0]))
+plt.plot(x,y/norm, linewidth=1, color='r')
+count, bins, ignored = plt.hist(x_walk, 1000, normed=True)
 #plt.scatter(x,d)
-plt.plot(x,y)
+#plt.plot(x,y)
 plt.savefig("Graphy.png")
